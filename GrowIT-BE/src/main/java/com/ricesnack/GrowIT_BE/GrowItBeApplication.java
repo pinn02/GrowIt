9c0ff0bdce26c93b1c9d01644c0b2b70bf0c099a
박시은 <psieun0606@gmail.com>
2025-09-05 10:45:36 +0900

    backend initial commit

---
+++ GrowIT-BE/src/main/java/com/ricesnack/GrowIT_BE/GrowItBeApplication.java
@@ -0,0 +1,13 @@
+package com.ricesnack.GrowIT_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowItBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowItBeApplication.class, args);
+	}
+
+}
---
+++ GrowIT-BE/src/test/java/com/ricesnack/GrowIT_BE/GrowItBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ricesnack.GrowIT_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowItBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
