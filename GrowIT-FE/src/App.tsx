b2e96e9c6df05db739662cf3518b1a568aac6df4
김민우 <pinn02@naver.com>
2025-09-08 11:28:27 +0900

    revert: frontend revert to init

--- GrowIT-FE/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import './App.css'
-
-function App() {
-  return (
-    <div>로그인</div>
-  )
-}
-
-export default App
--- GrowIT-FE/src/src/app/router.tsx
+++ GrowIT-FE/src/src/app/router.tsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom'
-import MainPage from '../pages/MainPage'
+import HomePage from '../pages/Home'
+
 export const router = createBrowserRouter([
-  { path: '/', element: <MainPage /> },
+  { path: '/', element: <HomePage /> },
 ])
