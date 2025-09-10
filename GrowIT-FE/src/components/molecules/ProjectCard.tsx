58752b9763c2d3b5e4d94123486ef8db34b4055e
kimseonwoo <dora01622@naver.com>
2025-09-10 15:41:32 +0900

    feat: 프로젝트 카드 컴포넌트 및 버튼 완성

--- GrowIT-FE/src/components/molecules/ProjectCard.tsx
+++ GrowIT-FE/src/components/molecules/ProjectCard.tsx
@@ -3,7 +3,7 @@ import ProjectButton from "../atoms/ProjectButton"
 
 function ProjectCard() {
   return (
-    <div className="relative mx-3 w-[30%]">
+    <div className="relative mx-3 w-[31%]">
       <img src={ProjectCardImage} alt="프로젝트 카드" className="w-full h-auto object-contain" />
 
       <div className="absolute inset-0 flex items-center justify-center">
