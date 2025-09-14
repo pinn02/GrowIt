992f9695650157540aca9537e07dbcc70246c3b4
김선우 <dora01622@naver.com>
2025-09-14 22:37:56 +0900

    feat: 게이지바 생성, 리포트 버튼 제거

--- GrowIT-FE/src/components/molecules/GameDataInformation.tsx
+++ GrowIT-FE/src/components/molecules/GameDataInformation.tsx
@@ -1,5 +1,8 @@
 import GameData from '../atoms/GameData'
 import { useGameDataStore } from '../../stores/gameDataStore'
+import coinIcon  from '../../assets/icons/coin.png'
+import productivityIcon  from '../../assets/icons/productivity.png'
+import enterpriseIcon  from '../../assets/icons/enterprise.png'
 
 function GameDataInformation() {
   const { enterpriseValue, productivity, finance, turn, currentProject } = useGameDataStore();
@@ -7,16 +10,16 @@ function GameDataInformation() {
   return (
     <div className='w-full flex items-center'>
       <div className='flex items-center gap-6 mx-3'>
-        <p className='text-xl font-bold mx-3 text-nowrap'>{turn} 턴</p>
-        <div className='w-full'>
-          <p className='text-nowrap'>진행 프로젝트: </p>
-          <p className='text-nowrap'>{currentProject ? currentProject : "진행 중인 프로젝트 없음"}</p>
+        <p className='text-sm font-bold text-nowrap'>{turn} 턴</p>
+        <div className='w-full flex'>
+          <p className='text-nowrap text-sm'>진행 프로젝트: </p>
+          <p className='text-nowrap text-sm'>{currentProject ? currentProject : "없음"}</p>
         </div>
       </div>     
       <div className='flex items-center h-full'>
-        <GameData dataName='기업가치' dataValue={ enterpriseValue } />
-        <GameData dataName='생산성' dataValue={ productivity } />
-        <GameData dataName='현재 자금' dataValue={ finance } />
+        <GameData dataName='기업가치' icon={enterpriseIcon} dataValue={enterpriseValue} dataMax={1000000} fillColor="bg-[#2D2F33]" />
+        <GameData dataName='생산성' icon={productivityIcon} dataValue={productivity} dataMax={100} fillColor="bg-[#B374F3]" />
+        <GameData dataName='자본' icon={coinIcon} dataValue={finance} dataMax={5000000} fillColor="bg-[#F8C545]" />
       </div>
     </div>
   )
--- GrowIT-FE/src/components/organisms/InformationBar.tsx
+++ GrowIT-FE/src/components/organisms/InformationBar.tsx
@@ -2,12 +2,11 @@ import { useState } from 'react'
 import Logo from "../atoms/Logo"
 import GameDataInformation from "../molecules/GameDataInformation"
 import TurnEndButton from "../atoms/Button"
-import ReportButton from "../atoms/Button"
 import ReportModal from "./ReportModal"
+import skyBackgroundImage from "../../assets/background_images/sky_page_background_image.png"
 
 const logoHeight = 48;
 const turnEndButtonSize = 100;
-const reportButtonSize = 100;
 
 function InformationBar() {
   const [showReportModal, setShowReportModal] = useState(false)
@@ -17,33 +16,28 @@ function InformationBar() {
   }
 
   const handleCloseModal = () => {
-    setShowReportModal(false)
+   setShowReportModal(false)
   }
 
   return (
     <>
-      <header className="bg-gray-400 h-16 flex items-center justify-between">
-        <Logo height={logoHeight} />
-        <div className="flex items-center">
-          <GameDataInformation />
-          <ReportButton
-            maxSize={reportButtonSize}
-            className="bg-green-300 text-black px-2 py-2 rounded hover:bg-green-400 transition-colors mx-2"
-            to="/report"
-          >
-            리포트
-          </ReportButton>
-          <TurnEndButton
-            maxSize={turnEndButtonSize}
-            className="bg-blue-300 text-black px-2 py-2 rounded hover:bg-blue-400 transition-colors mx-2"
-            onClick={handleTurnEnd}
-          >
+      <header
+        className="h-16 flex items-center justify-between px-4 bg-cover bg-center"
+        style={{ backgroundImage: `url(${skyBackgroundImage})` }}
+      >
+      <Logo height={logoHeight} />
+      <div className="flex items-center space-x-4">
+        <GameDataInformation />
+        <TurnEndButton
+          maxSize={turnEndButtonSize}
+          className="bg-transparent border-2 border-white text-white px-4 py-2 rounded-lg font-semibold shadow-md hover:bg-blue-700 transition-colors"
+          onClick={handleTurnEnd}
+        >
             턴 종료
           </TurnEndButton>
         </div>
       </header>
 
-      {/* showReportModal이 true일 때만  ReportModal가 렌더링 되도록 */}
       {showReportModal && (<ReportModal onClose={handleCloseModal} />)}
     </>
   )
