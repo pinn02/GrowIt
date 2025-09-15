a3bb36a254fe659626d5718dc6746f32292309c6
김민우 <pinn02@naver.com>
2025-09-15 10:29:28 +0900

    feat: 로그인 axios 함수 구현

---
+++ GrowIT-FE/src/stores/userStore.tsx
@@ -0,0 +1,17 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+type userState = {
+    token: string
+}
+
+export const useUserStore = create<userState>()(
+  persist(
+    (set) => ({
+      token: ""
+    }),
+    {
+      name: "user-storage",
+    }
+  )
+)
