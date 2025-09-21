3c49f5b5af4ade44dfb0fef07621c4c88ee56666
DESKTOP-DLOPF19\SSAFY <skil1489@naver.com>
2025-09-22 08:50:57 +0900

    refactor(data): sql 문 수정

--- GrowIT-BE/src/main/java/com/ricesnack/GrowIT_BE/machinelearning/domain/AnnualData.java
+++ GrowIT-BE/src/main/java/com/ricesnack/GrowIT_BE/machinelearning/domain/AnnualData.java
@@ -3,7 +3,6 @@ package com.ricesnack.GrowIT_BE.machinelearning.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Getter;
 
 import java.lang.reflect.Field;
 
@@ -14,48 +13,102 @@ public class AnnualData {
     @Id
     private int year;
 
-    private double unemployment_rate;
-    private double BSI_Composite;
-    private double real_wage_growth;
-    private double growth_rate_qoq_lag1;
+    private double unemploymentRate;
+    private double bsiComposite;
+    private double realWageGrowth;
+    private double growthRateQoqLag1;
     private double population;
-    private double GFCF_ICT_Real;
-    private double unemployment_rate_MA3;
-    private double BSI_Composite_MA3;
-    private double unemployment_rate_change3;
-    private double BSI_Composite_change3;
-    private double unemployment_rate_MA6;
-    private double BSI_Composite_MA12;
-    private double real_wage_growth_change12;
-    private double unemployment_rate_std3;
-    private double BSI_Composite_std6;
-    private double GDP;
-    private double ICT_Production;
-    private double ICT_Investment;
-    private double CCSI;
-    private double Corporate_Loan_Rate;
-    private double Equipment_Investment_Index;
-    private double productivity_index;
-    private double Exchange_Rate;
-    private double BSI_6M_MA;
-    private double GDP_12M_MA;
-    private double Exchange_Rate_12M_Std;
-    private double CCSI_3M_Momentum;
-    private double ICT_Prod_6M_Momentum;
-    private double Loan_Rate_Lag_3M;
-    private double Ad_Sentiment_DI;
-    private double manufacturing_productivity;
-
-    protected AnnualData() {}
-
-    public int getYear() {
-        return year;
-    }
+    private double gfcfIctReal;
+    private double unemploymentRateMa3;
+    private double bsiCompositeMa3;
+    private double unemploymentRateChange3;
+    private double bsiCompositeChange3;
+    private double unemploymentRateMa6;
+    private double bsiCompositeMa12;
+    private double realWageGrowthChange12;
+    private double unemploymentRateStd3;
+    private double bsiCompositeStd6;
+    private double gdp;
+    private double ictProduction;
+    private double ictInvestment;
+    private double ccsi;
+    private double corporateLoanRate;
+    private double equipmentInvestmentIndex;
+    private double productivityIndex;
+    private double exchangeRate;
+    private double bsi6MMa;
+    private double gdp12MMa;
+    private double exchangeRate12MStd;
+    private double ccsi3MMomentum;
+    private double ictProd6MMomentum;
+    private double loanRateLag3M;
+    private double manufacturingProductivity;
+    private double applicationLag1;
+    private double applicationLag2;
+    private double corpDomesticApplications;
+    private double corpDomesticRegistrations;
+    private double loanAvg;
+    private double loanAvgLag1;
+    private double loanAvgLag2;
+    private double registrationLag1;
+    private double registrationLag2;
+    private double regularAvgWage;
+    private double rndTotal;
+    private double rndTotalByIndustryAppSw;
+    private double rndTotalByIndustryConsulting;
+    private double rndTotalByIndustryEmbeddedSw;
+    private double rndTotalByIndustryInfoSvc;
+    private double rndTotalByIndustryProgrammingSvc;
+    private double rndTotalByIndustrySystemSw;
+    private double rndTotalByIndustryVirtualAsset;
+    private double rndTotalLag1;
+    private double rndTotalLag1X;
+    private double rndTotalLag1Y;
+    private double rndTotalLag2;
+    private double rndTotalLag2X;
+    private double rndTotalLag2Y;
+    private double serviceProductivity;
+    private double corporateLoansLaggedRate;
+    private double corporateLoansLaggedRateLag6;
+    private double corporateLoansLongRate;
+    private double gfcfIctRealLongGfcfReal;
+    private double gfcfRealMa6GfcfReal;
+    private double gfcfRealMa6GfcfReal6yMa;
+    private double ictImportsUsd;
+    private double ictExportsUsd;
+    private double infoCommValue;
+    private double invFinalElectricSeasonal;
+    private double invFinalElectricRaw;
+    private double invFinalComputerSeasonal;
+    private double invFinalComputerRaw;
+    private double invFinalSpecialMachineSeasonal;
+    private double invFinalSpecialMachineRaw;
+    private double invLaggedElectricSeasonal;
+    private double invLaggedComputerSeasonal;
+    private double invLaggedSpecialMachineSeasonal;
+    private double invLaggedElectricRaw;
+    private double invLaggedComputerRaw;
+    private double invLaggedSpecialMachineRaw;
+    private double invLongElectricSeasonal;
+    private double invLongComputerSeasonal;
+    private double invLongSpecialMachineSeasonal;
+    private double invLongElectricRaw;
+    private double invLongComputerRaw;
+    private double invLongSpecialMachineRaw;
+    private double ma6Electric;
+    private double ma6Computer;
+    private double ma6SpecialMachine;
+    private double ma6yElectric;
+    private double ma6yComputer;
+    private double ma6ySpecialMachine;
+    private double totalAssets;
+    private double totalInvestment;
+    private double cpi;
 
     public double getFeatureValue(String featureName) {
+        String camelCaseFeatureName = toCamelCase(featureName);
         try {
-            String fieldName = toCamelCase(featureName);
-            Field field = this.getClass().getDeclaredField(fieldName);
+            Field field = this.getClass().getDeclaredField(camelCaseFeatureName);
             field.setAccessible(true);
             return (double) field.get(this);
         } catch (NoSuchFieldException | IllegalAccessException e) {
@@ -63,23 +116,22 @@ public class AnnualData {
         }
     }
 
-    private String toCamelCase(String s){
-        if (s == null || !s.contains("_")) {
-            return s;
+    private String toCamelCase(String snakeCase) {
+        StringBuilder camelCase = new StringBuilder();
+        boolean nextIsUpper = false;
+        for (char c : snakeCase.toCharArray()) {
+            if (c == '_') {
+                nextIsUpper = true;
+            } else {
+                if (nextIsUpper) {
+                    camelCase.append(Character.toUpperCase(c));
+                    nextIsUpper = false;
+                } else {
+                    camelCase.append(c);
+                }
+            }
         }
-        String[] parts = s.split("_");
-        StringBuilder camelCaseString = new StringBuilder(parts[0]);
-        for (int i = 1; i < parts.length; i++){
-            camelCaseString.append(toProperCase(parts[i]));
-        }
-        return camelCaseString.toString();
-    }
-
-    private String toProperCase(String s) {
-        if (s == null || s.isEmpty()) {
-            return s;
-        }
-        return s.substring(0, 1).toUpperCase() +
-                s.substring(1).toLowerCase();
+        return camelCase.toString();
     }
 }
+
