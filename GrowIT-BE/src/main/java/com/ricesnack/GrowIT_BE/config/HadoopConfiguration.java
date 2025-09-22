48e3fc049da67c2080350d26dfbeec3c2230fc24
DESKTOP-DLOPF19\SSAFY <skil1489@naver.com>
2025-09-22 17:34:04 +0900

    feat(hadoop): 하둡 설정 만들기

---
+++ GrowIT-BE/src/main/java/com/ricesnack/GrowIT_BE/config/HadoopConfiguration.java
@@ -0,0 +1,22 @@
+package com.ricesnack.GrowIT_BE.config;
+
+import org.apache.hadoop.fs.FileSystem;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.net.URI;
+
+@Configuration
+public class HadoopConfiguration {
+
+    @Value("${hadoop.uri}")
+    private String hadoopUri;
+
+    @Bean
+    public FileSystem fileSystem() throws IOException {
+        org.apache.hadoop.conf.Configuration conf = new org.apache.hadoop.conf.Configuration();
+        return FileSystem.get(URI.create(hadoopUri), conf);
+    }
+}
