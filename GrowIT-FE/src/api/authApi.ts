d8ed4f3555bd1fe8645964b05101485d6a6cff12
kimseonwoo <dora01622@naver.com>
2025-09-24 15:15:52 +0900

    feature: 스토어 UI 크기 확대 및 반응형으로 개선

--- GrowIT-FE/src/api/authApi.ts
+++ GrowIT-FE/src/api/authApi.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = 'https://j13c201.p.ssafy.io';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
